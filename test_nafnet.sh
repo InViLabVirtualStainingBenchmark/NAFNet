e749670af5bbef5bda83f7ef33c64dabb9b0de80
edobor Aisosa <aisosa.edoboryemi@student.kdg.be>
2026-06-10 00:17:48 +0200

    created hpc folder

--- test_nafnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CONFIG=${1:-options/test/BCI/NAFNet-BCI-512-100k-test.yml}
-export PYTHONPATH=/code/NAFNet:/usr/local/lib64/python3.9/site-packages:$PYTHONPATH
-cd /code/NAFNet
-python3 basicsr/test.py \
-    -opt $CONFIG \
-    --launcher none \
-    2>&1 | tee /output/test_log.txt
